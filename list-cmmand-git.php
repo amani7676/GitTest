1e1886dbbd438aab5274a3edb5020f9b800f2362
farshid amani <f.amani.dev@gmail.com>
2025-05-07 08:23:10 +0330

    add ignorefile

--- list-cmmand-git.php
+++ list-cmmand-git.php
@@ -7,3 +7,9 @@
 // git branch (name branch) ==> create
 
 // git commit -a -m "" =>>>  a میاد کار Add رو انجام میده
+
+//git checkout -b (name branch) ==> اول میسازه بعدش خود سوسیچ میکنه
+
+//git merge branch => برنچ مقصد نوشته میشه یعنی محتویات ان برنچ داخل برنج مبدا
+
+//git branch -D (name) ==> همان برانچ فعلی نمیشه باید برنچی که داخلش میستید نویسید ت حذف بسشه
